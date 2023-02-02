edc279eb4661421af7a9f84629b8e3c544f2148e
teodoraruvceski <70577499+teodoraruvceski@users.noreply.github.com>
2023-02-02 12:24:54 +0100

    initial commit

---
+++ testing/src/components/home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+function Home(){
+    return(
+        <div>
+            <p>
+                Enter name:
+            </p>
+            <input type='text' placeholder="Name..."></input>
+            <button>Send</button>
+        </div>
+    )
+}
+export default Home;
